7ccdfaf9bf30952e47e8d5eaedc09e375d3ea48f
Leah Young <leah.young21@alumni.colostate.edu>
2023-11-10 14:58:46 -0800

    create social media component for icons, add asset files for bio section

---
+++ app/components/SocialMedia.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { BiLogoInstagram } from "react-icons/bi";
+import { BiLogoFacebookSquare } from "react-icons/bi";
+import { BiLogoTiktok } from "react-icons/bi";
+import { BiLogoYoutube } from "react-icons/bi";
+import { BiLogoPatreon } from "react-icons/bi";
+import { BiLogoSpotify } from "react-icons/bi";
+import { BiLogoApple } from "react-icons/bi";
+
+
+
+
+const SocialMedia = () => {
+    return (
+        <div>
+            <BiLogoInstagram color={"darkerPink"} />
+            <BiLogoFacebookSquare />
+            <BiLogoTiktok />
+            <BiLogoYoutube />
+            <BiLogoPatreon />
+            <BiLogoSpotify />
+            <BiLogoApple />
+        </div>
+    )
+}
+
+export default SocialMedia;
