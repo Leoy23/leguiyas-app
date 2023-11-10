b7165cefca68d16c1e4a51a60a3e916124673989
Leah Young <leah.young21@alumni.colostate.edu>
2023-11-10 14:59:25 -0800

    remove next api and create express server, update bio page and fetch from express server

--- app/components/Bio.tsx
+++ app/components/Bio.tsx
@@ -1,11 +1,26 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import bioHeading from "@/public/bio.png";
+import leguiya from "@/public/leguiya-about.png";
 
 const Bio = () => {
+    const [artistInfo, setArtistInfo] = useState<{ bio: string; socialMedia: object } | {}>({});
+
+    useEffect(() => {
+        fetch("http://localhost:8080/api/artist")
+            .then((res) => res.json())
+            .then((data) => console.log(data))
+    }, [])
+
     return (
         <div id="bio">
             <Image src={bioHeading} alt="bio heading" width={375} height={375} priority />
-            <h1>This is the bio section.</h1>
+            <div>
+                <p>{(artistInfo as { bio: string; socialMedia: object }).bio}</p>
+                <Image src={leguiya} alt="photo of leguiya" priority />
+            </div>
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
-"use client";
-
-import React from 'react';
-import { useEffect, useState } from 'react';
 import Bio from './components/Bio';
 import Contact from './components/Contact';
 import Music from './components/Music';
@@ -11,14 +7,19 @@ import Videos from './components/Videos';
 import Footer from './components/Footer';
 
 
+
 // type ArtistData = Array<{
 //   name: string,
 //   title: string,
 //   bio: string,
 // }>;
 
-const Home = () => {
+const Home = async () => {
 
+  const res = await fetch("http://localhost:8080/api/artist")
+  const data = await res.json();
+
+  console.log(data.data, "here")
 
   return (
     <div>
@@ -34,18 +35,3 @@ const Home = () => {
 }
 
 export default Home;
-
-
-// const artist: ArtistData = [{
-//   name: "Leguiya",
-//   title: "Leguiya's World",
-//   bio: "Music has the power to transport us to another time and place. Leguiya harnesses that power with music and melodies that appeal to audiences of music lovers and fellow professionals alike. Since a young age, they have found great joy and satisfaction in making music and sharing it with their dedicated fans. Find out more about them Below."
-// }]
-
-// useEffect(() => {
-//   fetch("http://localhost:8080/api/artist")
-//     .then((res) => res.json())
-//     .then((data) => {
-//       setData(data.name)
-//     })
-// }, [])
